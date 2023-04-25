1fedab7847d152005acd448512789371522dfb13
Ronald Norwood <ronnienorwood@live.com>
2023-04-25 09:14:24 -0700

    added user registration

--- jokes/views.py
+++ jokes/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 from .models import Joke
 from .forms import JokeForm
@@ -13,18 +14,30 @@ class JokeDetailView(DetailView):
     model = Joke
 
 
-class JokeCreateView(CreateView):
+class JokeCreateView(LoginRequiredMixin, CreateView):
     model = Joke
     # fields = ['question', 'answer']
     form_class = JokeForm
 
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
 
-class JokeUpdateView(UpdateView):
+
+class JokeUpdateView(UserPassesTestMixin, UpdateView):
     model = Joke
     # fields = ['question', 'answer']
     form_class = JokeForm
 
+    def test_func(self):
+        obj = self.get_object()
+        return self.request.user == obj.user
 
-class JokeDeleteView(DeleteView):
+
+class JokeDeleteView(UserPassesTestMixin, DeleteView):
     model = Joke
     success_url = reverse_lazy('jokes:list')
+
+    def test_func(self):
+        obj = self.get_object()
+        return self.request.user == obj.user
