f4fc36817812430a4b9cd59b9dcbf565300eba23
Ronald Norwood <ronnienorwood@live.com>
2023-04-25 10:21:37 -0700

    added messages

--- djangojokes/settings.py
+++ djangojokes/settings.py
@@ -63,8 +63,6 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
-MESSAGE_LEVEL = 10  # DEBUG
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
--- jokes/views.py
+++ jokes/views.py
@@ -1,6 +1,8 @@
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib import messages
 
 from .models import Joke
 from .forms import JokeForm
@@ -14,20 +16,20 @@ class JokeDetailView(DetailView):
     model = Joke
 
 
-class JokeCreateView(LoginRequiredMixin, CreateView):
+class JokeCreateView(SuccessMessageMixin, LoginRequiredMixin, CreateView):
     model = Joke
-    # fields = ['question', 'answer']
     form_class = JokeForm
+    success_message = 'Joke create successful.'
 
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
 
 
-class JokeUpdateView(UserPassesTestMixin, UpdateView):
+class JokeUpdateView(SuccessMessageMixin, UserPassesTestMixin, UpdateView):
     model = Joke
-    # fields = ['question', 'answer']
     form_class = JokeForm
+    success_message = 'Joke update successful.'
 
     def test_func(self):
         obj = self.get_object()
@@ -37,6 +39,14 @@ class JokeUpdateView(UserPassesTestMixin, UpdateView):
 class JokeDeleteView(UserPassesTestMixin, DeleteView):
     model = Joke
     success_url = reverse_lazy('jokes:list')
+
+    def delete(self, request, *args, **kwargs):
+        result = super().delete(request, *args, **kwargs)
+        return result
+
+    def form_valid(self, form):
+        messages.success(self.request, 'Joke delete successful.')
+        return super().form_valid(form)
 
     def test_func(self):
         obj = self.get_object()
