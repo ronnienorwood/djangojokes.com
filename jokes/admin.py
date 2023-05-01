85e2b0a99643e544aa6307202eb3a785f1c35f4d
Ronald Norwood <ronnienorwood@live.com>
2023-05-01 13:55:40 -0700

    customized django admin

--- jokes/admin.py
+++ jokes/admin.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
+from common.admin import DjangoJokesAdmin
 
 from .models import Joke, Category, Tag, JokeVote
 
 
 @admin.register(Tag)
-class TagAdmin(admin.ModelAdmin):
+class TagAdmin(DjangoJokesAdmin):
     model = Tag
-    list_display = ['tag', 'created', 'updated']
+    list_display = ('tag', 'created', 'updated')
+    search_fields = ('tag',)
 
     def get_readonly_fields(self, request, obj=None):
         if obj:  # editing an existing object
@@ -15,9 +17,9 @@ class TagAdmin(admin.ModelAdmin):
 
 
 @admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
+class CategoryAdmin(DjangoJokesAdmin):
     model = Category
-    list_display = ['category', 'created', 'updated']
+    list_display = ('category', 'created', 'updated')
 
     def get_readonly_fields(self, request, obj=None):
         if obj:  # editing an existing object
@@ -26,20 +28,30 @@ class CategoryAdmin(admin.ModelAdmin):
 
 
 @admin.register(Joke)
-class JokeAdmin(admin.ModelAdmin):
+class JokeAdmin(DjangoJokesAdmin):
     model = Joke
-    list_display = ['question', 'created', 'updated']
+    list_display = ('question', 'created', 'updated')
+    search_fields = ('question', 'answer')
+    ordering = ('-updated',)
+    list_filter = ('updated', 'category', 'tags')
+    date_hierarchy = 'updated'
+    # Form Attributes
+    autocomplete_fields = ('tags', 'user')
+    radio_fields = {'category': admin.HORIZONTAL}
 
     def get_readonly_fields(self, request, obj=None):
         if obj:  # editing an existing object
-            return ('slug', 'created', 'updated')
+            return ('slug', 'created', 'updated', 'vote_summary')
         return ()
+
+    def vote_summary(self, obj):
+        return f'{obj.num_votes} votes. Rating: {obj.rating}.'
 
 
 @admin.register(JokeVote)
-class JokeVoteAdmin(admin.ModelAdmin):
+class JokeVoteAdmin(DjangoJokesAdmin):
     model = JokeVote
-    list_display = ['joke', 'user', 'vote']
+    list_display = ('joke', 'user', 'vote')
 
     def get_readonly_fields(self, request, obj=None):
         if obj:  # editing an existing object
